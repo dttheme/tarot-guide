b668d26e6eb6fba8c41cf893c38543f4f6b05286
dttheme <dttheme@gmail.com>
2025-01-06 20:23:11 -0500

    test

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,7 +5,9 @@ const Card = ({ card }) => {
   const [isFlipped, setIsFlipped] = useState(false);
 
   const getImgUrl = (name) => {
-    const path = `.${import.meta.env.DEV ? "/assets/" : ""}/cards/${name}.jpg`;
+    const path = `.${
+      import.meta.env.DEV ? "/assets" : "/src/assets"
+    }/cards/${name}.jpg`;
     return new URL(path, import.meta.url).href;
   };
 
